940e8e94e4c655755a72e5540b70e08b54cf903e
Aghyad-dado <113991700+Aghyad-dado@users.noreply.github.com>
2023-10-16 17:17:30 +0200

    ss

--- main.py
+++ main.py
@@ -1,73 +1,45 @@
-'''
-import os
-
-# Define the path to your data folder
-data_path = "C:/Users/PC/OneDrive/Desktop/Data-Mining-2/data/op_spam_v1.4"
-# Define the subfolder you want to access
-subfolder = 'negative_polarity'
-
-# Define the full path to the subfolder
-subfolder_path = os.path.join(data_path, subfolder)
-
-# Check if the subfolder exists
-if os.path.exists(subfolder_path):
-    print("Hello")
-else:
-    print(f"The subfolder '{subfolder}' does not exist.")
-import os
-
-# Define the path to your data folder
-data_path = "C:/Users/PC/OneDrive/Desktop/Data-Mining-2/data/op_spam_v1.4"
-
-# Define the subfolder you want to access
-subfolder = 'negative_polarity'
-
-# Define the full path to the subfolder
-subfolder_path = os.path.join(data_path, subfolder)
-
-# Check if the subfolder exists
-if os.path.exists(subfolder_path):
-    print("Hello")
-else:
-    print(f"The subfolder '{subfolder}' does not exist.")
-'''
-
 import os
 import pandas as pd
 
 # Define the path to your data folder
-data_path = "C:/Users/PC/OneDrive/Desktop/Data-Mining-2/data/op_spam_v1.4"
+data_path = "data/op_spam_v1.4/negative_polarity"
 
-# Define the subfolders you want to access
-subfolders = ['negative_polarity', 'positive_polarity']
+# Initialize empty lists to store the data for train and test sets
+train_data = []
+test_data = []
 
-# Initialize an empty list to store the data
-data = []
+# List of sub-subfolders (fold1 to fold5)
+sub_subfolders = ['fold1', 'fold2', 'fold3', 'fold4', 'fold5']
 
-# List of sub-subfolders (fold1 to fold4)
-sub_subfolders = ['fold1', 'fold2', 'fold3', 'fold4']
-
-# Loop through the subfolders
-for subfolder in subfolders:
-    # Define the full path to the subfolder
-    subfolder_path = os.path.join(data_path, subfolder)
+# Loop through the sub-subfolders
+for sub_subfolder in sub_subfolders:
+    # Define the full path to the sub-subfolder for deceptive
+    deceptive_subfolder_path = os.path.join(data_path, 'deceptive_from_MTurk', sub_subfolder)
+    # Define the full path to the sub-subfolder for truthful
+    truthful_subfolder_path = os.path.join(data_path, 'truthful_from_Web', sub_subfolder)
     
-    # Loop through the sub-subfolders
-    for sub_subfolder in sub_subfolders:
-        # Define the full path to the sub-subfolder
-        sub_subfolder_path = os.path.join(subfolder_path, 'deceptive_from_MTurk', sub_subfolder)
-        
-        # Loop through the files in the sub-subfolder
-        for file in os.listdir(sub_subfolder_path):
-            file_path = os.path.join(sub_subfolder_path, file)
+    # Loop through the files in the sub-subfolders
+    for folder_path in [deceptive_subfolder_path, truthful_subfolder_path]:
+        for file in os.listdir(folder_path):
+            file_path = os.path.join(folder_path, file)
             
-            # Read the contents of the file and append to the data list
+            # Read the contents of the file
             with open(file_path, 'r', encoding='latin1') as f:
                 content = f.read()
-                data.append({'Subfolder': subfolder, 'Content': content})
+                
+                # Append to the appropriate list based on sub_subfolder (fold5 goes to test set)
+                if sub_subfolder == 'fold5':
+                    test_data.append({'Content': content, 'Subfolder': os.path.basename(folder_path)})
+                else:
+                    train_data.append({'Content': content, 'Subfolder': os.path.basename(folder_path)})
 
-# Create a DataFrame
-df = pd.DataFrame(data)
+# Create DataFrames
+train_df = pd.DataFrame(train_data)
+test_df = pd.DataFrame(test_data)
 
-# Display the DataFrame
-print(len(df))
+# Display the DataFrames
+print("Train Set:")
+print(train_df.head(10),len(train_df))
+
+print("\nTest Set:")
+print(test_df.head(10),len(test_df))

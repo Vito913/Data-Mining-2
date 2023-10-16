115ba45f5234544c44beb50c0694ef57f46f5d37
Vitalicecila <vitalialice99@gmail.com>
2023-10-16 18:09:09 +0200

    update

--- main.py
+++ main.py
@@ -1,73 +1,50 @@
-'''
-import os
-
-# Define the path to your data folder
-data_path = "C:/Users/PC/OneDrive/Desktop/Data-Mining-2/data/op_spam_v1.4"
-# Define the subfolder you want to access
-subfolder = 'negative_polarity'
-
-# Define the full path to the subfolder
-subfolder_path = os.path.join(data_path, subfolder)
-
-# Check if the subfolder exists
-if os.path.exists(subfolder_path):
-    print("Hello")
-else:
-    print(f"The subfolder '{subfolder}' does not exist.")
-import os
-
-# Define the path to your data folder
-data_path = "C:/Users/PC/OneDrive/Desktop/Data-Mining-2/data/op_spam_v1.4"
-
-# Define the subfolder you want to access
-subfolder = 'negative_polarity'
-
-# Define the full path to the subfolder
-subfolder_path = os.path.join(data_path, subfolder)
-
-# Check if the subfolder exists
-if os.path.exists(subfolder_path):
-    print("Hello")
-else:
-    print(f"The subfolder '{subfolder}' does not exist.")
-'''
-
 import os
 import pandas as pd
 
-# Define the path to your data folder
-data_path = "C:/Users/PC/OneDrive/Desktop/Data-Mining-2/data/op_spam_v1.4"
+# Path to the data folder
+data_folder = 'data/op_spam_v1.4'
 
-# Define the subfolders you want to access
-subfolders = ['negative_polarity', 'positive_polarity']
+# Specify the first subfolder (neg)
+neg_subdirectory_path = os.path.join(data_folder, 'negative_polarity')
 
-# Initialize an empty list to store the data
-data = []
+# Initialize empty lists to store file paths and labels
+fold_1_to_4_files = []
+fold_5_files = []
+labels_1_to_4 = []
+labels_5 = []
 
-# List of sub-subfolders (fold1 to fold4)
-sub_subfolders = ['fold1', 'fold2', 'fold3', 'fold4']
+# Loop through the 2 subdirectories inside 'neg'
+for subdir in os.listdir(neg_subdirectory_path):
+    subdir_path = os.path.join(neg_subdirectory_path, subdir)
+    for fold in os.listdir(subdir_path):
+        fold_path = os.path.join(subdir_path, fold)
+        # Check if the item in the directory is a directory and starts with 'fold'
+        if os.path.isdir(fold_path) and fold.startswith('fold'):
+            for root, dirs, files in os.walk(fold_path):
+                for file in files:
+                    label = 0 if file[0].lower() == 't' else 1
+                    file_path = os.path.join(root, file)
+                    with open(file_path, 'r', encoding='utf-8') as file:
+                        file_content = file.read()
+                        if fold != 'fold5':
+                            fold_1_to_4_files.append(file_content)
+                            labels_1_to_4.append(label)
+                        else:
+                            fold_5_files.append(file_content)
+                            labels_5.append(label)
 
-# Loop through the subfolders
-for subfolder in subfolders:
-    # Define the full path to the subfolder
-    subfolder_path = os.path.join(data_path, subfolder)
-    
-    # Loop through the sub-subfolders
-    for sub_subfolder in sub_subfolders:
-        # Define the full path to the sub-subfolder
-        sub_subfolder_path = os.path.join(subfolder_path, 'deceptive_from_MTurk', sub_subfolder)
-        
-        # Loop through the files in the sub-subfolder
-        for file in os.listdir(sub_subfolder_path):
-            file_path = os.path.join(sub_subfolder_path, file)
-            
-            # Read the contents of the file and append to the data list
-            with open(file_path, 'r', encoding='latin1') as f:
-                content = f.read()
-                data.append({'Subfolder': subfolder, 'Content': content})
+# Create dataframes from the gathered file paths and labels
+df_train = pd.DataFrame({'Review Text': fold_1_to_4_files, 'Label': labels_1_to_4})
+df_test = pd.DataFrame({'Review Text': fold_5_files, 'Label': labels_5})
 
-# Create a DataFrame
-df = pd.DataFrame(data)
+print(labels_1_to_4)
+print(labels_5)
 
-# Display the DataFrame
-print(len(df))
+# Print first few rows of the dataframes
+print('Dataframe for fold1 to fold4:')
+print(df_train)
+
+print('Dataframe for fold5:')
+print(df_test)
+
+

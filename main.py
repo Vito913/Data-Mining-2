717689b49882dc87c4da8b7f7274478c6e2a9054
vito vekic <vitovekic913@gmail.com>
2023-10-28 21:41:23 +0200

    added some changes, but not yet enough

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.feature_extraction.text import CountVectorizer
-from itertools import chain
+from itertools import chain, product
 from sklearn.model_selection import KFold
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import GridSearchCV
@@ -31,6 +31,8 @@ drop_percent = 0.0015
 part_of_speech = True
 # Checks if we are using stemming
 stemming = True
+
+onlyWords = False
 
 # Path to the data folder
 data_folder = 'data/op_spam_v1.4'
@@ -119,14 +121,26 @@ df_test['Review Text'] = df_test['Review Text'].apply(remove_unnecessary).apply(
 
 ##create document-term matrix for train data -> rows = files and columns = terms
 # Extract words without POS from the 'Review Text' column
-words_only_reviews_train = [[' '.join(word for word, pos in review)] for review in df_train['Review Text']]
-words_only_reviews_test = [[' '.join(word for word, pos in review)] for review in df_test['Review Text']]
-# Flatten the list of words into a single list
-all_words_train = list(chain.from_iterable(words_only_reviews_train))
-all_words_test = list(chain.from_iterable(words_only_reviews_test))
+
+# Extract words without POS from the 'Review Text' column
+words = [[' '.join(word for word, pos in review)] for review in df_train['Review Text']]
+words = [[' '.join(word for word, pos in review)] for review in df_test['Review Text']]
+
+# Extract words with POS from the 'Review Text' column
+words_and_pos = [[' '.join(f"{word}_{pos}" for word, pos in review)] for review in df_train['Review Text']]
+words_and_pos = [[' '.join(f"{word}_{pos}" for word, pos in review)] for review in df_test['Review Text']]
+
+# Get all words from the training and test data
+all_words_train = list(chain.from_iterable(words))
+all_words_test = list(chain.from_iterable(words))
+
 # Convert the list of words into space-separated strings for each review
-doc_strings_train = [' '.join(review) for review in words_only_reviews_train]
-doc_strings_test = [' '.join(review) for review in words_only_reviews_test]
+doc_strings_train = [' '.join(review) for review in words]
+doc_strings_test = [' '.join(review) for review in words]
+
+# Convert the list of words with POS into space-separated strings for each review
+doc_strings_train_pos = [' '.join(review) for review in words_and_pos]
+doc_strings_test_pos = [' '.join(review) for review in words_and_pos]
 
 # Initialize the CountVectorizer
 vectorizerTrain = CountVectorizer()
@@ -137,19 +151,37 @@ vectorizerTest = CountVectorizer(vocabulary=vectorizerTrain.vocabulary_)
 # Transform the test documents into a document-term co-occurrence matrix using the same vocabulary
 doc_term_matrix_test = vectorizerTest.transform(doc_strings_test)
 
+# Initialize the CountVectorizer for POS tags
+vectorizerTrainPos = CountVectorizer()
+# Transform the documents into a document-term co-occurrence matrix
+doc_term_matrix_train_pos = vectorizerTrainPos.fit_transform(doc_strings_train_pos)
+# Initialize the CountVectorizer with the vocabulary from the training data
+vectorizerTestPos = CountVectorizer(vocabulary=vectorizerTrainPos.vocabulary_)
+# Transform the test documents into a document-term co-occurrence matrix using the same vocabulary
+doc_term_matrix_test_pos = vectorizerTestPos.transform(doc_strings_test_pos)
+
 # Convert the document-term matrix to an array
 doc_term_matrix_array_train = doc_term_matrix_train.toarray()
 doc_term_matrix_array_test = doc_term_matrix_test.toarray()
+doc_term_matrix_array_train_pos = doc_term_matrix_train_pos.toarray()
+doc_term_matrix_array_test_pos = doc_term_matrix_test_pos.toarray()
+
 if drop:
     doc_term_matrix_array_train = doc_term_matrix_array_train[:, (doc_term_matrix_array_train.sum(axis=0) >= drop_percent * doc_term_matrix_array_train.shape[0])]
+    doc_term_matrix_array_train_pos = doc_term_matrix_array_train_pos[:, (doc_term_matrix_array_train_pos.sum(axis=0) >= drop_percent * doc_term_matrix_array_train_pos.shape[0])]
 
 # Get the feature names (words) corresponding to the columns of the matrix
 feature_names_train = vectorizerTrain.get_feature_names_out()
+feature_names_train_pos = vectorizerTrainPos.get_feature_names_out()
 
 ## Check which words happen less than drop_percent times and remove them from the matrix
 docTermMatrixTrain = pd.DataFrame(doc_term_matrix_array_train, columns=feature_names_train, index=filenamesTrain)
+docTermMatrixTrainPos = pd.DataFrame(doc_term_matrix_array_train_pos, columns=feature_names_train_pos, index=filenamesTrain)
+
 # Ensure that the test matrix only includes words from the training matrix
 docTermMatrixTest = pd.DataFrame(doc_term_matrix_array_test, columns=feature_names_train, index=filenamesTest)
+docTermMatrixTestPos = pd.DataFrame(doc_term_matrix_array_test_pos, columns=feature_names_train_pos, index=filenamesTest)
+
 print(docTermMatrixTrain)
 print(docTermMatrixTest)
 
@@ -175,92 +207,113 @@ y = df_train['Label']
 # split the data into 10 folds and every time train on 9 and test on 1
 
 kf = KFold(n_splits=10, shuffle=True, random_state=42)
+def logistic_regression_classification(x, y, kf):
+    # Define the range of lambda values to try
+    Lambda = {'C': [1000.0, 2000.0, 3000.0, 4000.0, 5000.0, 6000.0, 7000.0, 8000.0, 9000.0]}
 
-# Define the range of lambda values to try "I am suspicious of the range of values still"
-Lambda = {'C': [1000.0, 2000.0, 3000.0, 4000.0, 5000.0, 6000.0, 7000.0, 8000.0, 9000.0]}
+    logistic_regression = LogisticRegression(penalty='l1', solver='liblinear')
 
+    grid_search = GridSearchCV(estimator=logistic_regression, param_grid=Lambda, cv=kf)
 
-logistic_regression = LogisticRegression(penalty='l1', solver='liblinear')
+    # Perform the grid search
+    grid_search.fit(x, y)
 
-grid_search = GridSearchCV(estimator = logistic_regression, param_grid = Lambda, cv=kf)
+    # Get the best hyperparameter
+    best_lambda = grid_search.best_params_['C']
 
-# Perform the grid search
-grid_search.fit(x, y)
+    # Get the best model
+    best_model = grid_search.best_estimator_
 
-# Get the best hyperparameter
-best_lambda = grid_search.best_params_['C']
+    # Get the best cross-validated score
+    best_accuracy = grid_search.best_score_
 
-# Get the best model
-best_model = grid_search.best_estimator_
-
-# Get the best cross-validated score
-best_accuracy = grid_search.best_score_
-
-print(best_accuracy, best_lambda)
+    return best_accuracy, best_lambda
 
 ########### CLASSIFICATION TREES #####################
 
-param_grid = {"ccp_alpha": np.linspace(0, 0.1, 11)}
-best_alpha = 0
-best_accuracy_2_electric_boogaloo = 0
+def decision_tree_classification(x, y, kf):
+    param_grid = {"ccp_alpha": np.linspace(0, 0.2, 20)}
+    best_alpha = 0
+    best_accuracy = 0
 
-for train_index, val_index in kf.split(x):
+    for train_index, val_index in kf.split(x):
 
-    X_train, X_val = x[train_index], x[val_index]
-    y_train, y_val = y[train_index], y[val_index]
+        X_train, X_val = x[train_index], x[val_index]
+        y_train, y_val = y[train_index], y[val_index]
 
-    clf = tree.DecisionTreeClassifier()
-    grid_search = GridSearchCV(clf, param_grid=param_grid)
-    grid_search.fit(X_train, y_train)
-    #print("best alpha:", grid_search.best_params_['ccp_alpha'])
-    best_clf = grid_search.best_estimator_
-    best_clf.fit(X_train, y_train)
-    y_pred = best_clf.predict(X_val)
-    accuracy = best_clf.score(X_val, y_val)
-    print("accuracy:", accuracy)
-    
-    # Active selection of best hyperparameters
-    param_grid["ccp_alpha"] = np.linspace(max(0, grid_search.best_params_['ccp_alpha']-0.01), 
-                                           min(0.1, grid_search.best_params_['ccp_alpha']+0.01), 11)
+        clf = tree.DecisionTreeClassifier()
+        grid_search = GridSearchCV(clf, param_grid=param_grid)
+        grid_search.fit(X_train, y_train)
+        best_clf = grid_search.best_estimator_
+        best_clf.fit(X_train, y_train)
+        y_pred = best_clf.predict(X_val)
+        accuracy = best_clf.score(X_val, y_val)
+        print("accuracy:", accuracy)
 
-    if accuracy > best_accuracy_2_electric_boogaloo:
-        best_accuracy_2_electric_boogaloo = accuracy
-        best_alpha = grid_search.best_params_['ccp_alpha']
-        
-print(best_accuracy_2_electric_boogaloo, best_alpha)
+        # Active selection of best hyperparameters
+        param_grid["ccp_alpha"] = np.linspace(max(0, grid_search.best_params_['ccp_alpha']-0.02), 
+                                               min(0.1, grid_search.best_params_['ccp_alpha']+0.02), 20)
 
+        if accuracy > best_accuracy:
+            best_accuracy = accuracy
+            best_alpha = grid_search.best_params_['ccp_alpha']
 
+    return best_accuracy, best_alpha
 ################ RANDOM FOREST #####################
 
-param_grid = {
-    'n_estimators': [200, 300, 400, 500, 600,700, 800, 900],  # List of different numbers of trees
-    'max_features': ['sqrt', 'log2', None]  # Different options for max_features
+def random_forest_classification(doc_term_matrix_array_train: pd.DataFrame, df_train: pd.DataFrame) -> Tuple[float, str, RandomForestClassifier]:
+    param_grid = {
+        'n_estimators': [200, 300, 400, 500, 600,700, 800, 900],  # List of different numbers of trees
+        'max_features': ['sqrt', 'log2', None]  # Different options for max_features
+    }
+    cv = KFold(n_splits=10, shuffle=True, random_state=42) 
+    rf = RandomForestClassifier(random_state=42)
+    grid_search = GridSearchCV(estimator=rf, param_grid=param_grid, cv=cv, n_jobs=-1)
+
+    #search for best params config using 10-fold cross validation
+    #for every parameters combo, model is trained (on 9 folds) and tested (on 1 fold) 10 times; val. accuracy is mean of all 10 trials per param combo!
+    start_time = time.time()
+    history = grid_search.fit(doc_term_matrix_array_train, df_train['Label'])
+    end_time = time.time()
+    print("time for greed search", start_time - end_time)
+
+    # Print validation accuracy for each parameter combination
+    print("Validation Accuracy for Each Parameter Combination:")
+    for params, test_score in zip(grid_search.cv_results_['params'], grid_search.cv_results_['mean_test_score']):
+        print(f"Parameters: {params}, Validation Accuracy: {test_score:.4f}")
+
+    #go on with the best config!
+    best_n_estimators = grid_search.best_params_['n_estimators']
+    best_max_features = grid_search.best_params_['max_features']
+    best_rf_model = grid_search.best_estimator_
+    #access best params
+    print("Best number of trees:", best_n_estimators)
+    print("Best max features:", best_max_features)
+    print("Best model", best_rf_model)
+
+    return best_n_estimators, best_max_features, best_rf_model
+
+
+
+
+#### LOOP OVER THIS ####
+_ , best_lambda = logistic_regression_classification(x, y, kf)
+best_n_estimators, best_max_features, best_rf_model = random_forest_classification(doc_term_matrix_array_train, df_train)
+_, best_alpha = decision_tree_classification(x, y, kf)
+
+
+
+for drop, drop_percent, part_of_speech, in product([True,False], [0.0015, 0.002, 0.0025, 0.003], [True, False]):
+    if part_of_speech:
+        doc_term_matrix_array_train = doc_term_matrix_array_train_pos
+        doc_term_matrix_array_test = doc_term_matrix_array_test_pos
+    else:
+        doc_term_matrix_array_train = doc_term_matrix_array_train
+        doc_term_matrix_array_test = doc_term_matrix_array_test
+    y = df_train['Label']
     
-}
-cv = KFold(n_splits=10, shuffle=True, random_state=42) 
-rf = RandomForestClassifier(random_state=42)
-grid_search = GridSearchCV(estimator=rf, param_grid=param_grid, cv=cv, n_jobs=-1)
-#serch for best params config using 10-fold cross validation
-#for every parameters combo, model is trained (on 9 folds) and tested (on 1 fold) 10 times; val. accuracy is mean of all 10 trials per param combo!
-start_time = time.time()
-history = grid_search.fit(doc_term_matrix_array_train, df_train['Label'])
-end_time = time.time()
-print("time for greed search", start_time - end_time)
-
-# Print validation accuracy for each parameter combination
-print("Validation Accuracy for Each Parameter Combination:")
-for params, test_score in zip(grid_search.cv_results_['params'], grid_search.cv_results_['mean_test_score']):
-    print(f"Parameters: {params}, Validation Accuracy: {test_score:.4f}")
 
 
-#go on with the best config!
-best_n_estimators = grid_search.best_params_['n_estimators']
-best_max_features = grid_search.best_params_['max_features']
-best_rf_model = grid_search.best_estimator_
-#access best params
-print("Best number of trees:", best_n_estimators)
-print("Best max features:", best_max_features)
-print("Best model", best_rf_model)
 
 
 ################ Naive Bayes without bigram nor features selection "Chi-square" #####################
